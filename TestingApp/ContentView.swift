e5defdf2926f3d2d3d2b04f52a7fbc91c82861e4
darshilagrawal <darshilagrawal2000@gmail.com>
2021-04-13 16:42:27 +0530

    Initial Commit

---
+++ TestingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestingApp
+//
+//  Created by Darshil Agrawal on 13/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestingApp/TestingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAppApp.swift
+//  TestingApp
+//
+//  Created by Darshil Agrawal on 13/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TestingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
